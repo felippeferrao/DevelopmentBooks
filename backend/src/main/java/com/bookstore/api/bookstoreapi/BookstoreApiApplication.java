fee7336cea097c0c02e7328573756515bf5bf7c1
Felippe Ferrão <56120911+felippeferrao@users.noreply.github.com>
2023-04-27 20:15:45 +0200

    Code to Make the tests Valid (TDD)

--- backend/src/main/java/com/bookstore/api/bookstoreapi/BookstoreApiApplication.java
+++ backend/src/main/java/com/bookstore/api/bookstoreapi/BookstoreApiApplication.java
@@ -1,5 +1,7 @@
 package com.bookstore.api.bookstoreapi;
 
+import javax.annotation.security.PermitAll;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +18,13 @@ public class BookstoreApiApplication {
 	}
 
 	@GetMapping("/index")
+	@PermitAll
 	public String index() {
 		return "Home Api Requested";
+	}
+	
+	@GetMapping("/unauthorized")
+	public String indexUnauthorized() {
+		return "Unauthorized Content";
 	}
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_app (name, email, password) VALUES
+  ('sa', 'sa@email.com', '$2a$10$yo9nRrztHF.58LZVIpnY0OD2A9CzIsZNQlXkMKyH9b9peLx4hDg7q');
