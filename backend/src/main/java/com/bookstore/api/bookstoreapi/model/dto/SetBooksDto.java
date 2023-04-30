a8ec6708de065c37029321bc96147a45c041fd4e
Felippe Ferrão <56120911+felippeferrao@users.noreply.github.com>
2023-05-01 01:23:39 +0200

    SetBooksDto

---
+++ backend/src/main/java/com/bookstore/api/bookstoreapi/model/dto/SetBooksDto.java
@@ -0,0 +1,18 @@
+package com.bookstore.api.bookstoreapi.model.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetBooksDto {
+    private BigDecimal totalPrice;
+    private BigDecimal totalDicount;
+    private List<List<SetDto>> bookSets;
+
+}
